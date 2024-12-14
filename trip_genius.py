5ee92213001c6d436b36fe61c6b1e935483fc79a
RATTANDEEP SINGH <merattandeep@gmail.com>
2024-12-13 21:29:58 -0500

    Optimizations in Code (#7)

---
+++ trip_genius.py
@@ -0,0 +1,12 @@
+from bookings_script import initiate_bookings
+import json
+
+# Example usage
+if __name__ == "__main__":
+    sample_query = "Book me a trip to New York from 20th December 2024 to 5th January 2025. I am currently located at New Delhi."
+    
+    results = initiate_bookings(sample_query)
+
+    booking_params = results['booking_params']
+    print("\nFlight Booking Results:")
+    print(json.dumps(booking_params, indent=2))
