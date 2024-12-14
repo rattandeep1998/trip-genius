91865c384e825b5372943c222ee190202a2bb0ec
RATTANDEEP SINGH <merattandeep@gmail.com>
2024-12-13 23:34:01 -0500

    Run evals (#8)

--- trip_genius.py
+++ trip_genius.py
@@ -3,7 +3,7 @@ import json
 
 # Example usage
 if __name__ == "__main__":
-    sample_query = "Book me a trip to New York from 20th December 2024 to 5th January 2025. I am currently located at New Delhi."
+    sample_query = "Book me a trip for Mr. Rattandeep Singh born on 7th March 1998 to New York from 20th December 2024 to 5th January 2025. I am currently located at New Delhi."
     
     results = initiate_bookings(sample_query)
 
